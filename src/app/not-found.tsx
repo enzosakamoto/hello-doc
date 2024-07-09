8122e1820b4817bc6d0020ba864536b31288a4c9
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-09 10:06:42 -0300

    feat: Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        404 - Page not found
+      </h1>
+    </main>
+  )
+}
